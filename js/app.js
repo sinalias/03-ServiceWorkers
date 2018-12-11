5a21e38a94d6a94470e03575f3ce4a1da4d014d3
Roberto Perez <sinalias@gmail.com>
2018-12-11 20:02:45 +0100

    avances en el curso

--- js/app.js
+++ js/app.js
@@ -2,5 +2,27 @@
 
 // Detectar si podemos usar Service Workers
 if ( navigator.serviceWorker ) {
-    navigator.serviceWorker.register('/sw.js');
+    navigator.serviceWorker.register('/sw.js')
+            .then(reg=>{
+
+                Notification.requestPermission().then(result =>{
+                    console.log(result);
+                    reg.showNotification('Hola mundo!');
+                    
+                });
+                // setTimeout(()=>{
+
+                //     reg.sync.register('posteo-gatitos');
+                //     console.log('Se enviaron fotos de gatito al servidor');
+                // }, 3000);
+            });
+
+    
+    
 }
+
+// fetch('https://reqres.in/api/users')
+// .then(resp => resp.text())
+// .then(console.log);
+
+
--- sw.js
+++ sw.js
@@ -27,3 +27,21 @@ self.addEventListener('activate', event =>{
     // eliminamos chache vieja
     console.log('SW: Service worker activo y listo para recibir nuevas modificaciones 2');
 });
+
+self.addEventListener('fetch', event =>{
+    
+    //Aplicar estrategias del cache
+    //console.log('WS:', event.request.url);
+
+});
+
+// SYNC: Recuperamos la conexion a internet
+self.addEventListener('sync', event =>{
+    console.log('Tenemos conexion');
+    console.log(event.tag);
+});
+
+// PUSH: Manejar las push notifications
+self.addEventListener('push', event =>{
+    console.log('Notificacion recibida');
+});
